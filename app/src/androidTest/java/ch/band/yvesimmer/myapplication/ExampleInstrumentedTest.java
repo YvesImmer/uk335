1f4e6a87313a2418b63f068af7c5f7125903ef08
Iram <inkonito67@gmail.com>
2021-01-26 15:44:46 +0100

    Ein Kommentar gemacht

--- app/src/androidTest/java/ch/band/yvesimmer/myapplication/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/ch/band/yvesimmer/myapplication/ExampleInstrumentedTest.java
@@ -22,5 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("ch.band.yvesimmer.myapplication", appContext.getPackageName());
+        //Ein Kommentar
     }
 }
