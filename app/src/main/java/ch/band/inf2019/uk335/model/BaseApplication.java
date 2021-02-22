c296878070a2b35139618a3863fba7a61b92187b
Iram <inkonito67@gmail.com>
2021-02-22 09:34:22 +0100

    Added Application class

---
+++ app/src/main/java/ch/band/inf2019/uk335/model/BaseApplication.java
@@ -0,0 +1,10 @@
+package ch.band.inf2019.uk335.model;
+
+import android.app.Application;
+
+public class BaseApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
